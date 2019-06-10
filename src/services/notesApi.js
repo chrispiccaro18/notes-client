577ec6660ba370dd5fbab676ca506513c076f93e
Chris Piccaro <chrispiccaro18@gmail.com>
2019-06-10 16:09:02 -0700

    hooking up back end

--- src/services/notesApi.js
+++ src/services/notesApi.js
@@ -1,6 +1,7 @@
+import { post } from './request';
+
+const BASE_PATH = '/api/v1/notes';
+
 export const createNote = note => {
-  return Promise.resolve({
-    _id: '7890',
-    ...note
-  });
+  return post(`${BASE_PATH}`, note);
 };
