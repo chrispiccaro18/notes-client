817bfd0166b70e7bd96e1d7fa0ac4ba073e0a835
Chris Piccaro <chrispiccaro18@gmail.com>
2019-06-10 17:17:03 -0700

    fetch notes action and get notes api

--- src/services/notesApi.js
+++ src/services/notesApi.js
@@ -1,7 +1,11 @@
-import { post } from './request';
+import { post, get } from './request';
 
 const BASE_PATH = '/api/v1/notes';
 
 export const createNote = note => {
   return post(`${BASE_PATH}`, note);
+};
+
+export const getNotes = () => {
+  return get(BASE_PATH);
 };
